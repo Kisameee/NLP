77a63fde26acd4672a6a3c1b33b6b00a76665cb5
Clementine <clementine.thornary@gmail.com>
2018-10-26 12:16:39 +0200

    Create parser class

--- amazon/documents/amazonReviewParser.py
+++ amazon/documents/amazonReviewParser.py
@@ -1,3 +1,7 @@
+from amazon.documents import Document
+from amazon.documents.parser import Parser
+
+
 class AmazonReviewParser(Parser):
     def read(self, content: str) -> Document:
         """Reads the content of an amazon data file and returns one document instance per document it finds."""
---
+++ amazon/documents/parser.py
@@ -0,0 +1,9 @@
+class Parser():
+
+    def __init__(self) -> None:
+        """
+        Constructor of the Document class
+        """
+        self.text = None
+        self.tokens = None
+        self.sentences = None
