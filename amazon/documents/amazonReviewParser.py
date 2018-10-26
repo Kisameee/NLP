2c5041b746759ec2fefd5f33a9abf8db025e5290
Clementine <clementine.thornary@gmail.com>
2018-10-26 14:59:23 +0200

    correcting

--- amazon/documents/amazonReviewParser.py
+++ amazon/documents/amazonReviewParser.py
@@ -7,6 +7,11 @@ class AmazonReviewParser(Parser):
         """Reads the content of an amazon data file and returns one document instance per document it finds."""
         import json
         documents = []
+        all_json = json.load(content)
+        for comment in all_json:
+            doc = Document.create_from_text(comment['review_text'])
+            doc.overall = comment['overall']
+            documents.append(doc)
         #documents.append(create_from_text())
         # Split lines and loop over them
         # Read json with: data = json.loads(line)
--- amazon/tests/documents/test_parser.py
+++ amazon/tests/documents/test_parser.py
@@ -9,6 +9,3 @@ class TestParser(TestCase):
 
     def test_read_file(self, file = os.path.join(DATA_DIR, 'digital_music_reviews.json')):
         self.assertEqual(len(AmazonReviewParser().read_file(file)), 64706, "Not enough documents")
-
-
-        BOUHAHAHAHAHAH
