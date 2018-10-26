ea22e095a1d6ebb3ef7aace98a974e8cb78a687a
Ghiles <ghiles.cherfaoui@soat.fr>
2018-10-26 15:01:14 +0200

    testing parser

--- amazon/tests/documents/test_parser.py
+++ amazon/tests/documents/test_parser.py
@@ -8,4 +8,4 @@ from amazon.documents.amazonReviewParser import AmazonReviewParser
 class TestParser(TestCase):
 
     def test_read_file(self, file=os.path.join(DATA_DIR, 'digital_music_reviews.json')):
-        self.assertEqual(len(AmazonReviewParser().read_file(file)), 64706, "Not enough documents")
+        self.assertEqual(len(AmazonReviewParser().read_file(file)), 64706, "Not enough documents...")
