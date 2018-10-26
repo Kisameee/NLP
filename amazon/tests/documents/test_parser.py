e3ed5aa8626faa8d40935004fd30fbe768ec4b1d
HernandezPierre <pierre.hernandez77940@gmail.com>
2018-10-26 12:42:58 +0200

    test_parser add

--- amazon/tests/documents/test_parser.py
+++ amazon/tests/documents/test_parser.py
@@ -6,4 +6,3 @@ class TestParser(TestCase):
     def test_parser_json_is_true(self):
         file = json.load("/Users/HERNANDEZPierre/PycharmProjects/NLP/amazon/data/digital_music_reviews.json")
 
-        self.assertEqual(file.
