e192ee7e3176668e093c0fee155be21c139e181e
HernandezPierre <pierre.hernandez77940@gmail.com>
2018-10-26 12:16:58 +0200

    ajout code class parser

--- amazon/documents/parser.py
+++ amazon/documents/parser.py
@@ -1,9 +1,13 @@
-class Parser():
+class Parser(object):
+    """Classe parente pour tous les parsers"""
+    def create(self):
+        return self
 
-    def __init__(self) -> None:
-        """
-        Constructor of the Document class
-        """
-        self.text = None
-        self.tokens = None
-        self.sentences = None
+    def read_file(self, filename: str) -> Document:
+        with open(filename, 'r', encoding='utf-8') as fp:
+            content = fp.read()
+        return self.read(content)
+
+class SimpleTextParser(Parser):
+    def read(self, content: str) -> Document:
+        return Document().create_from_text(content)
