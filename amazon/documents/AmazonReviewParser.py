584d3ef28bd731fda21caa5cf2c457b98ebe73f7
Clementine <clementine.thornary@gmail.com>
2018-10-26 12:11:56 +0200

    Add init for datas and parser beginning

---
+++ amazon/documents/AmazonReviewParser.py
@@ -0,0 +1,10 @@
+class AmazonReviewParser(Parser):
+    def read(self, content: str) -> Document:
+        """Reads the content of an amazon data file and returns one document instance per document it finds."""
+        import json
+        documents = []
+        # Split lines and loop over them
+        # Read json with: data = json.loads(line)
+        # Instantiate Document object from "reviewText" and label from "overall"
+
+        return documents
