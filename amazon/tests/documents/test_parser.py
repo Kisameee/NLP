256e6ef88469e246a9ec3493d3780389f602060c
Clementine <clementine.thornary@gmail.com>
2018-10-26 12:47:14 +0200

    test parser generique

--- amazon/tests/documents/test_parser.py
+++ amazon/tests/documents/test_parser.py
@@ -1,8 +1,11 @@
 import json
+import os
 from unittest import TestCase
+
+from amazon.data import DATA_DIR
 
 
 class TestParser(TestCase):
     def test_parser_json_is_true(self):
-        file = json.load("/Users/HERNANDEZPierre/PycharmProjects/NLP/amazon/data/digital_music_reviews.json")
+        file = json.load(os.join(DATA_DIR, 'digital_music_reviews.json'))
 
