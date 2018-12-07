b93604b4c5da9845dcfec0dd802f152d8ba659a7
Clementine <clementine.thornary@gmail.com>
2018-12-07 16:04:13 +0100

    test ok vectorizer

--- amazon/tests/documents/test_vectorize.py
+++ amazon/tests/documents/test_vectorize.py
@@ -8,10 +8,10 @@ from amazon.documents.amazonReviewParser import AmazonReviewParser
 
 class TestVectorizer(TestCase):
 
-    def test_vectorise(self):
-        document = (AmazonReviewParser().read_file(filename=os.path.join(DATA_DIR, 'digital_music_reviews.json')))
+    def test_vectorize(self):
+        document = AmazonReviewParser().read_file(os.path.join(DATA_DIR, 'test.json'))
         v = Vectorizer(os.path.join(DATA_DIR, 'glove.txt'))
         words, pos, shapes = v.encode_features(document)
-        self.assertEqual(words.tolist() == [[13075, 85, 805]])
-        self.assertEqual(pos.tolist() == [[38, 11, 21]])
-        self.assertEqual(shapes.tolist() == [[4, 5, 2]])
+        self.assertEqual(len(words.tolist()), 13, "mauvaise liste mots")
+        self.assertEqual(len(pos.tolist()), 13, "mauvaise liste pos")
+        self.assertEqual(len(shapes.tolist()), 13, "mauvaise shape list")
